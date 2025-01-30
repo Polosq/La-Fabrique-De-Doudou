3579ea79a76b554d2573913087ccaf537d2d5d3d
Polosq <paadurlien@gmail.com>
2025-01-30 20:54:18 +0100

    a

--- Scripts/script.js
+++ Scripts/script.js
@@ -8,6 +8,36 @@ let divsala = document.getElementById("divsala");
 let divchat = document.getElementById("divchat");
 let divnoel = document.getElementById("divnoel");
 
+let titrehlwn = document.getElementById("titrehlwn");
+let titrenoel = document.getElementById("titrenoel");
+let titrepaques = document.getElementById("titrepaques");
+let titrefl = document.getElementById("titrefl");
+let titrepokemon = document.getElementById("titrepokemon");
+
+function menu(e){
+    $(titrehlwn).removeClass("titreclic");
+    $(titrenoel).removeClass("titreclic");
+    $(titrepaques).removeClass("titreclic");
+    $(titrefl).removeClass("titreclic");
+    $(titrepokemon).removeClass("titreclic");
+    if (this.className=="titreclic"){
+        $(titrehlwn.parentNode).removeClass("invisible");
+        $(titrenoel.parentNode).removeClass("invisible");
+        $(titrepaques.parentNode).removeClass("invisible");
+        $(titrefl.parentNode).removeClass("invisible");
+        $(titrepokemon.parentNode).removeClass("invisible");
+        $(this).removeClass("titreclic");
+    }else{
+        $(titrehlwn.parentNode).addClass("invisible");
+        $(titrenoel.parentNode).addClass("invisible");
+        $(titrepaques.parentNode).addClass("invisible");
+        $(titrefl.parentNode).addClass("invisible");
+        $(titrepokemon.parentNode).addClass("invisible");
+        $(this).addClass("titreclic");
+        $(this.parentNode).removeClass("invisible");
+    }
+}
+
 
 function audioplay(e){
     if(this.id=="divpika"){
@@ -29,3 +59,8 @@ divsala.addEventListener("mouseover", audioplay);
 divchat.addEventListener("mouseover", audioplay);
 divnoel.addEventListener("mouseover", audioplay);
 
+titrehlwn.addEventListener("click", menu);
+titrenoel.addEventListener("click", menu);
+titrepaques.addEventListener("click", menu);
+titrefl.addEventListener("click", menu);
+titrepokemon.addEventListener("click", menu);
